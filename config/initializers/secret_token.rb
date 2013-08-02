98b33a22c16e56d8f6dade0ff27d2aafb946bc8c
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2013-08-02 22:20:06 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geonotifications::Application.config.secret_key_base = '2e82ed7e3df6b9751686829df77a222d2870c253e8ede321c820b08eee8acc6f3c98dfee1393da9a1dab1b57736541f5d771b0e925ddc52d8f5abe564d3814ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Geonotifications::Application.config.session_store :cookie_store, key: '_geonotifications_session'
