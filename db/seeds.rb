895e1e0d8435558eaa3669448d9d9d6a229a8518
knut Johannes Dahle <knut.johannes.dahle@gmail.com>
2013-08-03 11:39:48 +0200

    Added relation between User and Poi, and added a test user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
 location = [59.1356, 9.6]
 
+unless User.where(email: 'user@test.no').first
+  user = User.create(email: 'user@test.no', password: 'test', password_confirmation: 'test')
+  user.confirm!
+  puts "created #{user.inspect} with password test"
+end
+
 unless Poi.where(location: location).first
-  poi = Poi.create(location: location, category: 'estate_for_sale', title: 'funkisbolig', description: 'med fabuloso utsikt!')
+  poi = Poi.create(location: location, category: 'estate_for_sale', title: 'funkisbolig', description: 'med fabuloso utsikt!', created_by: User.first)
   puts "created #{poi.inspect}"
 end
