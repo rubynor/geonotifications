a98ffba25b7ee1e79af2294ee69359ceebffa1f3
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2013-08-04 12:31:13 +0200

    installed devise views in app. added name to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 location = [59.1356, 9.6]
 
 unless User.where(email: 'user@test.no').first
-  user = User.create(email: 'user@test.no', password: 'test', password_confirmation: 'test')
+  user = User.create(name: 'John Doe', email: 'user@test.no', password: 'test', password_confirmation: 'test')
   user.confirm!
   puts "created #{user.inspect} with password test"
 end
